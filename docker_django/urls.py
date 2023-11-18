2e1113746b2d0cfb47cc1ed4b8d6c4e2925bd310
rakeshnayal-k <rakeshdravid25@gmail.com>
2023-11-18 19:21:59 +0530

    small changes

--- docker_django/urls.py
+++ docker_django/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('devops/', include("devops.urls")),
+    path('devops/', include("app_devops.urls")),
 
 
 ]
